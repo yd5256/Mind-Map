d9cd93ebce8c89f44ee725ed9d00b8d5b58683b5
Alex Cheng <0119acheng@gmail.com>
2025-10-25 19:18:51 -0400

    map page exists

--- project/main.py
+++ project/main.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 from . import db
+from flask import request, redirect, url_for, flash
 
 main = Blueprint('main', __name__)
 
@@ -8,7 +9,22 @@ main = Blueprint('main', __name__)
 def index():
   return render_template('index.html')
 
+@main.route('/map', methods=['GET', 'POST'])
+def map_page():
+  if request.method == 'POST':
+    # simple handling: get submitted topic and (placeholder) redirect back to map
+    topic = request.form.get('topic')
+    if not topic:
+      flash('Please enter a topic', 'warning')
+      return redirect(url_for('main.map_page'))
+    # TODO: generate or process mind map for `topic`; for now re-render map with topic
+    return render_template('map.html', topic=topic)
+
+  return render_template('map.html')
+
 @main.route('/profile')
 @login_required
 def profile():
   return render_template('profile.html', name=current_user.name)
+
+
