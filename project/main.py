1a5c7ae4b69e1f0dfd5af114dff4c362ecf94b56
Yavuz Damkaci <yavuzdamkaci@Yavuzs-MacBook-Pro.local>
2025-10-25 19:21:29 -0400

    some updates to form stuff

--- project/main.py
+++ project/main.py
@@ -1,6 +1,11 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 from . import db
+from flask_wtf import FlaskForm
+from wtforms.validators import InputRequired, Length
+
+class BigIdeaForm(FlaskForm):
+    idea = StringField('big_idea', validators=[InputRequired(), Length(min=1, max=500)])
 
 main = Blueprint('main', __name__)
 
@@ -12,3 +17,8 @@ def index():
 @login_required
 def profile():
   return render_template('profile.html', name=current_user.name)
+
+@main.route('/map', methods = ['GET', 'POST'])
+def map():
+  
+  return render_template('map.html')
