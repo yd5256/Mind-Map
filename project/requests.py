d90b3d4d50cedfb8a5257d0639945023eb0edb1b
lee-andr <leeandr@bc.edu>
2025-10-25 17:08:37 -0400

    get and post requests

--- project/requests.py
+++ project/requests.py
@@ -2,4 +2,16 @@ import requests, json
 
 api_key = #
 agent_url = #
-run_id = requests.get(agent_url)
+run_id = requests.post(agent_url)
+
+
+get_response = requests.get(f"{agent_url}/{run_id.text}", headers={"x-api-key": api_key})
+
+
+if (get_response.status_code == 200): 
+        response = get_response.response
+
+elif (get_response.status_code == 204):
+        response = get_response.message
+
+
