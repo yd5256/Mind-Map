0a9230fcd41789e4277875f880f187feede4355f
Yavuz Damkaci <yavuzdamkaci@Yavuzs-MacBook-Pro.local>
2025-10-25 19:40:54 -0400

    form stuff updated

--- project/main.py
+++ project/main.py
@@ -1,12 +1,13 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_required, current_user
 from . import db
 from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
 from wtforms.validators import InputRequired, Length
 
 class BigIdeaForm(FlaskForm):
-    idea = StringField('big_idea', validators=[InputRequired(), Length(min=1, max=500)])
-from flask import request, redirect, url_for, flash
+    topic = StringField('Topic', validators=[InputRequired(), Length(min=1, max=500)])
+    submit = SubmitField('Generate Mind Map')
 
 main = Blueprint('main', __name__)
 
@@ -16,25 +17,18 @@ def index():
 
 @main.route('/map', methods=['GET', 'POST'])
 def map_page():
-  if request.method == 'POST':
-    # simple handling: get submitted topic and (placeholder) redirect back to map
-    topic = request.form.get('topic')
-    if not topic:
-      flash('Please enter a topic', 'warning')
-      return redirect(url_for('main.map_page'))
-    # TODO: generate or process mind map for `topic`; for now re-render map with topic
-    return render_template('map.html', topic=topic)
-
-  return render_template('map.html')
+    form = BigIdeaForm()
+    
+    if form.validate_on_submit():
+        topic = form.topic.data
+        # TODO: generate or process mind map for `topic`
+        return render_template('map.html', form=form, topic=topic)
+    
+    return render_template('map.html', form=form)
 
 @main.route('/profile')
 @login_required
 def profile():
   return render_template('profile.html', name=current_user.name)
-
-@main.route('/map', methods = ['GET', 'POST'])
-def map():
-  
-  return render_template('map.html')
 
 
